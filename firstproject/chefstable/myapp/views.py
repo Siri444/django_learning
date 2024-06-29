5bab42a36d1db6a2f43eafca27f51b611676a82f
unknown <sukkalasirichandana4444@gmail.com>
2024-06-29 11:25:22 +0530

    adding files

---
+++ firstproject/chefstable/myapp/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    return HttpResponse("Hello welcome to the restraunt")
+#string formatting and http response
+def details(request):
+    path= request.path
+    scheme=request.scheme
+    method=request.method
+    address=request.META['REMOTE_ADDR']
+    user_agent = request.META['HTTP_USER_AGENT']
+    path_info=request.path_info 
+    
+    msg=f"""<br>
+        <br>path:{path} 
+        <br>scheme:{scheme}
+        <br>method:{method}
+        <br>address:{address}
+        <br>useragent:{user_agent}
+        <br>pathinfo:{path_info}
+    """
+    response=HttpResponse(msg)
+    return response
+
+#working with urlparameters
+def getuser(requet,name,id):
+    return HttpResponse("Name:{} userid:{}".format(name,id))
+
+#quering strings
+
+def qryview(request):
+    name=request.GET['name']
+    id=request.GET['id']
+    return HttpResponse("Name:{} userid:{}".format(name,id))
+        
